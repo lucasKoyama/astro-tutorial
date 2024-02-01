15712496d94e7ffc1206b16bb575b3c13e485449
Lucas Koyama <lucaskoyamahhh@gmail.com>
2024-02-01 17:28:56 -0300

    integrated React component in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-lucaskoyama.netlify.app/"
+  site: "https://astro-lucaskoyama.netlify.app/",
+  integrations: [preact()]
 });
