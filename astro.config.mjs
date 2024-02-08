a930249f1a96021ca58757abe5d98dcb4eb49bf5
Lucas Koyama <lucaskoyamahhh@gmail.com>
2024-02-08 18:35:49 -0300

    added explanations about astro rendering modes features

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-lucaskoyama.netlify.app/",
+  site: "https://lucaskoyama-astronaut.netlify.app/about/",
   integrations: [preact()]
 });
