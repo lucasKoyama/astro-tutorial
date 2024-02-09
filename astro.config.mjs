9c8dc5eae9162e9834612e5803405fdbc43000c9
Lucas Koyama <lucaskoyamahhh@gmail.com>
2024-02-09 16:34:23 -0300

    added prefetching code to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucaskoyama-astronaut.netlify.app/about/",
-  integrations: [preact()]
+  integrations: [preact()],
+  prefetch: { prefetchAll: true } // Project using View Transitions make prefetch enabled by default, but I've inserted this for learning purposes
 });
